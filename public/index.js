3c16882c99ef139dd766a1ce12822e337b1ae3c0
osiyo <merewall@gmail.com>
2021-08-23 15:29:04 -0400

    commenting out code in index.js

--- public/index.js
+++ public/index.js
@@ -1,11 +1,18 @@
+// call function for homepage loading
 init();
 
+// function for homepage load
 async function init() {
+  // use URL to check for search parameter of workout id, and if undefined...
   if (location.search.split("=")[1] === undefined) {
+    // use API request to get last workout from database
     const workout = await API.getLastWorkout();
+    // if a last workout is found in database,
     if (workout) {
+      // set the workout's id to the search parameter in URL
       location.search = "?id=" + workout._id;
-    } else {
+    } // otherwise, if no workout in database, don't display "Continue Workout" button
+      else {
       document.querySelector("#continue-btn").classList.add("d-none")
     }
   }
