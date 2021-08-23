a070c5dc1f676850fc77290f75435bacc9aefb4d
osiyo <merewall@gmail.com>
2021-08-23 16:11:12 -0400

    starting routes for API requests

---
+++ routes/api.js
@@ -0,0 +1,36 @@
+const router = require("express").Router();
+const {Workout, Exercise} = require("../models");
+
+// router.post("/api/transaction", ({ body }, res) => {
+//   Transaction.create(body)
+//     .then(dbTransaction => {
+//       res.json(dbTransaction);
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// });
+
+// router.post("/api/transaction/bulk", ({ body }, res) => {
+//   Transaction.insertMany(body)
+//     .then(dbTransaction => {
+//       res.json(dbTransaction);
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// });
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .populate("exercises")
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
