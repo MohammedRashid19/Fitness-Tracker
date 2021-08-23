76c397c4d54103f67b2d6d2d5cf65c75921ea4ce
osiyo <merewall@gmail.com>
2021-08-23 15:24:42 -0400

    commenting out code in exercise.js

--- public/exercise.js
+++ public/exercise.js
@@ -1,3 +1,4 @@
+// variables for selecting HTML elements on /exercise page
 const workoutTypeSelect = document.querySelector("#type");
 const cardioForm = document.querySelector(".cardio-form");
 const resistanceForm = document.querySelector(".resistance-form");
@@ -17,13 +18,17 @@ const newWorkout = document.querySelector(".new-workout")
 let workoutType = null;
 let shouldNavigateAway = false;
 
+// function for /exercise page load
 async function initExercise() {
   let workout;
-
+  // if this is a new workout
   if (location.search.split("=")[1] === undefined) {
+    // send API request to create a new workout
     workout = await API.createWorkout()
     console.log(workout)
   }
+  // using newly created workout or existing workout, 
+  // set url with workout id as search parameter
   if (workout) {
     location.search = "?id=" + workout._id;
   }
@@ -32,9 +37,10 @@ async function initExercise() {
 
 initExercise();
 
+// function for changing workout type
 function handleWorkoutTypeChange(event) {
   workoutType = event.target.value;
-
+  // changing the display: none class (d-none) depending on which chosen type of workout
   if (workoutType === "cardio") {
     cardioForm.classList.remove("d-none");
     resistanceForm.classList.add("d-none");
@@ -45,47 +51,53 @@ function handleWorkoutTypeChange(event) {
     cardioForm.classList.add("d-none");
     resistanceForm.classList.add("d-none");
   }
-
+  // make sure inputs are valid using function below
   validateInputs();
 }
 
+// function to validate user's chosen inputs
 function validateInputs() {
   let isValid = true;
 
+  // validation for resistance workouts
   if (workoutType === "resistance") {
+    // name of workout is required (cannot be empty string)
     if (nameInput.value.trim() === "") {
       isValid = false;
     }
-
+    // weights are required
     if (weightInput.value.trim() === "") {
       isValid = false;
     }
-
+    // sets are required
     if (setsInput.value.trim() === "") {
       isValid = false;
     }
-
+    // reps are required
     if (repsInput.value.trim() === "") {
       isValid = false;
     }
-
+    // duration is required
     if (resistanceDurationInput.value.trim() === "") {
       isValid = false;
     }
-  } else if (workoutType === "cardio") {
+  } // validation for cardio workouts
+    else if (workoutType === "cardio") {
+    // name is required input
     if (cardioNameInput.value.trim() === "") {
       isValid = false;
     }
-
+    // duration is required input 
     if (durationInput.value.trim() === "") {
       isValid = false;
     }
-
+    // distance is required input
     if (distanceInput.value.trim() === "") {
       isValid = false;
     }
   }
-
+  // if all inputs for workout are valid,
+  // enable buttons to "complete" the workout or "add exercise"
   if (isValid) {
     completeButton.removeAttribute("disabled");
     addButton.removeAttribute("disabled");
@@ -95,17 +107,21 @@ function validateInputs() {
   }
 }
 
+// function for handling exercise/workout form submission
 async function handleFormSubmit(event) {
   event.preventDefault();
 
+  // empty object to hold workout stats
   let workoutData = {};
 
+  // setting stats for cardio workout from user inputs
   if (workoutType === "cardio") {
     workoutData.type = "cardio";
     workoutData.name = cardioNameInput.value.trim();
     workoutData.distance = Number(distanceInput.value.trim());
     workoutData.duration = Number(durationInput.value.trim());
-  } else if (workoutType === "resistance") {
+  } // setting stats for resistance workout from user inputs
+    else if (workoutType === "resistance") {
     workoutData.type = "resistance";
     workoutData.name = nameInput.value.trim();
     workoutData.weight = Number(weightInput.value.trim());
@@ -114,11 +130,14 @@ async function handleFormSubmit(event) {
     workoutData.duration = Number(resistanceDurationInput.value.trim());
   }
 
+  // send API request using exercise stats input by user
   await API.addExercise(workoutData);
+  // clear form inputs
   clearInputs();
   toast.classList.add("success");
 }
 
+// 
 function handleToastAnimationEnd() {
   toast.removeAttribute("class");
   if (shouldNavigateAway) {
@@ -126,6 +145,7 @@ function handleToastAnimationEnd() {
   }
 }
 
+// helper function to clear all form inputs
 function clearInputs() {
   cardioNameInput.value = "";
   nameInput.value = "";
@@ -137,20 +157,27 @@ function clearInputs() {
   weightInput.value = "";
 }
 
+// event listener for changing the workout type & initiate function for handling
 if (workoutTypeSelect) {
   workoutTypeSelect.addEventListener("change", handleWorkoutTypeChange);
 }
+// event listener for complete workout button
 if (completeButton) {
   completeButton.addEventListener("click", function (event) {
+    // allows navigation back to home page via handleToastAnimationEnd function
     shouldNavigateAway = true;
+    // process all form data using helper function
     handleFormSubmit(event);
   });
 }
+// event listener for adding another exercise, capturing form data
 if (addButton) {
   addButton.addEventListener("click", handleFormSubmit);
 }
+// event listener to navigate back to homepage after "Workout Added Successfully!" animation is over
 toast.addEventListener("animationend", handleToastAnimationEnd);
 
+// validate all the inputs on the page user helper function
 document
   .querySelectorAll("input")
   .forEach(element => element.addEventListener("input", validateInputs));
