7955fbb125046e330d5b7e6b1976ebea646e70d3
osiyo <merewall@gmail.com>
2021-08-24 03:09:48 -0400

    adding async await to connect to mongoose database

--- server.js
+++ server.js
@@ -10,15 +10,34 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workout',
-  {
-    useNewUrlParser:true,
-    useCreateIndex:true,
-    useFindAndModify:false,
-    useUnifiedTopology: true
-  }
-);
+// mongoose.connect(
+//   process.env.MONGODB_URI || 'mongodb://localhost/workout',
+//   {
+//     useNewUrlParser:true,
+//     useCreateIndex:true,
+//     useFindAndModify:false,
+//     useUnifiedTopology: true
+//   }
+// );
+const connectDb = async () => {
+  await mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser:true,
+      useCreateIndex:true,
+      useFindAndModify:false,
+      useUnifiedTopology: true 
+    }
+  )
+  .then(() => {
+    console.log("Connected to database!")
+  })
+  .catch(() => {
+    console.log(err)
+  })
+}
+
+connectDb().catch(err => console.log(err))
 
 // routes
 app.use(require("./routes/api.js"));
