0cf4a92eb754e0f4b5606350b1b5e453a550234c
osiyo <merewall@gmail.com>
2021-08-24 14:42:53 -0400

    commenting out and cleaning server.js

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
+// BRING IN MONGOOSE DATABASE AND EXPRESS SERVER
 const express = require("express");
 const mongoose = require("mongoose");
 
+// VARIABLE FOR PORT
 const PORT = process.env.PORT || 3000
 
+// INITIATE INSTANCE OF EXPRESS SERVER AS YOUR APP
 const app = express();
 
+// DATA PARSING
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// PUBLIC DIRECTORY ACCESS
 app.use(express.static("public"));
 
-// mongoose.connect(
-//   process.env.MONGODB_URI || 'mongodb://localhost/workout',
-//   {
-//     useNewUrlParser:true,
-//     useCreateIndex:true,
-//     useFindAndModify:false,
-//     useUnifiedTopology: true
-//   }
-// );
-// ADDING ASYNC AWAIT FOR DATABASE CONNECTION
+// ASYNC AWAIT FOR MONGOOSE DATABASE CONNECTION
 // CONSULTED CODE HERE: https://stackoverflow.com/questions/54890608/how-to-use-async-await-with-mongoose
 const connectDb = async () => {
   await mongoose.connect(
@@ -38,12 +34,13 @@ const connectDb = async () => {
     console.log(err)
   })
 }
-
+// CONNECT TO MONGOOSE DATABASE OR CATCH & CONSOLE LOG ERROR
 connectDb().catch(err => console.log(err))
 
-// routes
+// ROUTES
 app.use(require("./routes/api.js"));
 
+// START LISTENING
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
